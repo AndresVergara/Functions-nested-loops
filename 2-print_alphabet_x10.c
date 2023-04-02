6ed3aa7d99d20ea21e50dea76359e721213f325a
root <root@DESKTOP-IE1RA48>
2023-04-02 18:22:38 -0500

    queso

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+void print_alphabet_x10(void){
+    char a, b;
+    for (b = 1; b <= 10; b++){
+        for (a = 'a'; a <= 'z'; a++){
+        _putchar (a);
+        
+        }
+    _putchar ('\n');
+    }    
+}
